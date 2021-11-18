a18e597e8bcbf7e491eb16e6f798ef9165b2df75
nikhilpandey-dev <nikhilpandey4@gmail.com>
2021-11-18 19:02:10 +0530

    Removed TODOs from the write.py.

--- write.py
+++ write.py
@@ -25,7 +25,6 @@ def write_to_csv(results, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
 
     with open(filename, 'w') as outfile:
         writer = csv.DictWriter(outfile, fieldnames=list(fieldnames))
@@ -63,7 +62,6 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
     output_rows_list = list()
     with open(filename, 'w') as outfile:
         output_row_dict = dict()
@@ -82,7 +80,6 @@ def write_to_json(results, filename):
             output_neo_dict['potentially_hazardous'] = element.neo.hazardous
             output_row_dict['neo'] = output_neo_dict
             output_rows_list.append(output_row_dict)
-        
         json.dump(output_rows_list, outfile, allow_nan=True)
 
 
