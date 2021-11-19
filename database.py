3211dff0b29f899a28381e7b26aa30892ec0fa5e
nikhilpandey-dev <nikhilpandey4@gmail.com>
2021-11-19 12:02:20 +0530

    Did pycodestyle and pydocstyle checks on database.py and completed that.

--- database.py
+++ database.py
@@ -17,12 +17,12 @@ from models import NearEarthObject, CloseApproach
 class NEODatabase:
     """A database of near-Earth objects & close approaches.
 
-    
     A `NEODatabase` contains a collection of NEOs and a collection of close
     approaches. It additionally maintains a few auxiliary data structures to
     help fetch NEOs by primary designation or by name and to help speed up
     querying for close approaches that match criteria.
     """
+
     def __init__(self, neos: NearEarthObject, approaches: CloseApproach):
         """Create a new `NEODatabase`.
 
